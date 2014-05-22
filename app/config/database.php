8f089e26e06b5ca477fcb7a4f0cf319a248ab510
willmkt <will@MacBook-Pro-de-Willan.local>
2014-05-22 00:57:33 -0400

    change db namo

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'evento',
+			'database'  => 'forge',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
